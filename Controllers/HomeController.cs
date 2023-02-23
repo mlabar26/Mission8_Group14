e4d1a23a4573bf3771036e96fb5d9ec725e538d1
Marcus La Bar <mlabar26@byu.edu>
2023-02-23 08:54:15 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission8_Group14.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8_Group14.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/TaskForm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8_Group14.Models
+{
+    public class TaskForm
+    {
+    }
+}
